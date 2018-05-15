f8af7589634045a5fcb8b91b86a55dadab63142e
Alwin <alwinfirmansyah@gmail.com>
2018-05-14 20:42:38 -0700

    add cardBehavior to source control

---
+++ Animated SET/CardBehavior.swift
@@ -0,0 +1,59 @@
+//
+//  CardBehavior.swift
+//  Animated SET
+//
+//  Created by Alwin Firmansyah on 5/11/18.
+//  Copyright © 2018 Alwin Firmansyah. All rights reserved.
+//
+
+import UIKit
+
+class CardBehavior: UIDynamicBehavior
+{
+    lazy var collisionBehavior: UICollisionBehavior = {
+        let behavior = UICollisionBehavior()
+        behavior.translatesReferenceBoundsIntoBoundary = true
+        behavior.collisionMode = .boundaries
+        return behavior
+    }()
+    
+    lazy var itemBehavior: UIDynamicItemBehavior = {
+        let behavior = UIDynamicItemBehavior()
+        behavior.allowsRotation = true
+        behavior.elasticity = 1.0
+        behavior.resistance = 1.0
+        return behavior
+    }()
+    
+    private func push(_ item: UIDynamicItem) {
+        let push = UIPushBehavior(items: [item], mode: .instantaneous)
+        push.angle = (CGFloat.pi*2).arc4random
+        push.magnitude = CGFloat(3.0)
+        push.action = { [unowned push, weak self] in
+            self?.removeChildBehavior(push)
+        }
+        addChildBehavior(push)
+    }
+    
+    func addItem(_ item: UIDynamicItem) {
+        collisionBehavior.addItem(item)
+        itemBehavior.addItem(item)
+        push(item)
+    }
+    
+    func removeItem(_ item: UIDynamicItem) {
+        collisionBehavior.removeItem(item)
+        itemBehavior.removeItem(item)
+    }
+    
+    override init() {
+        super.init()
+        addChildBehavior(collisionBehavior)
+        addChildBehavior(itemBehavior)
+    }
+    
+    convenience init(in animator: UIDynamicAnimator) {
+        self.init()
+        animator.addBehavior(self)
+    }
+}
